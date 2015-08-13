c43176a0f56709b7c936e2632e261477f88f62aa
kaz <kmaciaszek@digisoft.tv>
2015-08-13 12:13:10 +0100

    Modal window example.

--- app/Home/HomeController.js
+++ app/Home/HomeController.js
@@ -1,5 +1,15 @@
 /* global angular */
 
-angular.module('myApp').controller('HomeController', [function() {
+function HomeController($rootScope, $scope, $location, $modal) {
   'use strict';
-}]);
+
+  $scope.modalExample = function() {
+    $modal.open({
+      templateUrl: 'User/createUser.html',
+      controller: CreateUserController
+    });
+  }
+
+};
+
+angular.module('myApp').controller('HomeController', HomeController);
--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@
      'myApp.services'
 ]);
 
-angular.module('myApp.directives', ['datatables']);
+angular.module('myApp.directives', ['datatables', 'ui.bootstrap']);
 angular.module('myApp.services', []);
 
 app.config(['$routeProvider', function($routeProvider) {
